5446e873550d3736f0ac4e77f35a82ad147106fe
DravaGen <sigulya2018@mail.ru>
2024-10-04 14:38:11 +0300

    fixed creation of the base directory

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,7 @@ from config import Config
 from .services import FileService
 
 
-if Config.BASE_DIRECTORY.exists() is False:
-    Config.BASE_DIRECTORY.mkdir(exist_ok=True)
+if not Config.BASE_DIRECTORY.exists():
+    Config.BASE_DIRECTORY.mkdir(parents=True, exist_ok=True)
 
 FileService.files_initialization()
